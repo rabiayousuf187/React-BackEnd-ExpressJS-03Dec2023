013a95a7422f51d91bb12ab87b3fd2fc00ab80b2
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-12-03 12:43:37 +0500

    installl nodemon

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ var app = express();
 
 //  node index.js to run file
 // Routes
-app.get('/', function (req, res) {
-res.send("Welocme to GeeksforGeeks!");
+// app.get('/', function (req, res) {
+// res.send("Welocme to GeeksforGeeks! GET METHOD");
+// });
+
+// POSt directly didnot hit post but get is used for this
+app.post('/', function (req, res) {
+res.send("Welocme to GeeksforGeeks! POST METHOD");
 });
+
 
 app.get('/about', function (req, res) {
 res.send("About Page");
